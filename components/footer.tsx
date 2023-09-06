1be513b9b6170eb7ff5df0d6ffc81baac30af962
devhumayun <humayunkabir9408@gmail.com>
2023-09-06 11:03:55 +0600

    Add footer section

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='text-center mb-5 text-lg text-grap-950'>
+      <small> &copy; 2023 Humayun. All Right Reserved. </small>
+    </section>
+  )
+}
+
+export default Footer
